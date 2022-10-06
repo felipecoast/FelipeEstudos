bc8d913bede7fe1c856ccd892e2999adada7b2a1
felipecoast <felipe.coast@gmail.com>
2022-10-06 20:12:01 -0300

    modal atraves da performsegue programatica

--- exercicioNavegacao/exercicioNavegacao/ViewController.swift
+++ exercicioNavegacao/exercicioNavegacao/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tappedScreen2Button(_ sender: UIButton) {
+        performSegue(withIdentifier: "GoToScreen2", sender: nil)
+    }
+    
 }
 
