3b85e33c46e78da0e776c2beebf7f66a93f68308
felipecoast <felipe.coast@gmail.com>
2022-11-07 23:04:41 -0300

    criada a parte de tableView

---
+++ TableViewCollectionView/TableViewCollectionView/TableViewCell/CarTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CarTableViewCell.swift
+//  TableViewCollectionView
+//
+//  Created by Felipe Almeida on 07/11/22.
+//
+
+import UIKit
+
+class CarTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    static let identifier: String = "CarTableViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: identifier, bundle: nil)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
